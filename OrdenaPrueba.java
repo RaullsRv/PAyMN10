d8e94efb5e4ff846111b8a75617d1f01248c2198
Adrián Rivera <adrianrueda37@gmail.com>
2016-04-21 20:17:13 -0500

    Added files via upload

---
+++ OrdenaPrueba.java
@@ -0,0 +1,12 @@
+package odenarprueba;
+
+public class OdenarPrueba {
+
+    public static void main(String[] args) {
+        Nombres nom = new Nombres();
+        nom.leerDatos();
+        nom.ordenar();
+        nom.imprimirInfo();
+    }
+
+}
